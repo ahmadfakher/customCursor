b93a5405bc2823bec48c8c5a06b11ba61d395af9
Ahmad Fakher Hassan <113045165+ahmadfakher@users.noreply.github.com>
2025-10-06 17:18:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const cursorCollection = document.getElementsByClassName("cursor");
+const links = document.getElementsByClassName("link");
+const customCursor = cursorCollection[0];
+
+Array.prototype.forEach.call(links, (link) => {
+    link.addEventListener("mouseenter", () => {
+        customCursor.classList.replace("cursor", "hover");
+    });
+
+    link.addEventListener("mouseleave", () => {
+        customCursor.classList.replace("hover", "cursor");
+    });
+});
+
+window.addEventListener("mousemove", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    customCursor.style.left = `${x}px`;
+    customCursor.style.top = `${y}px`;
+});
